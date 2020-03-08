5ad92c10ae8036d0195a4f2a0819c7213033df07
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-03-08 16:58:33 -0700

    Built form with add smurf functionality, added all actions/reducers

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers/reducer';
+import { createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers/reducer';
+import thunk from "redux-thunk";
 
-const store = createStore(reducer);
+const store = createStore(rootReducer, (applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById("root"));
 
--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,5 +1,32 @@
-export const reducer() {
-    return {
-      title: 'Hello world! I\'m in the Redux store!',
+import {
+    GET_SMURF,
+    GET_SUCCESS,
+    GET_FAIL,
+    ADD_SMURF,
+    ADD_SUCCESS,
+    ADD_FAIL
+  } from "../reducers/action";
+  
+  const initialState = {
+    messge: "",
+    smurfs: []
+  };
+  
+  export const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case GET_SMURF:
+        return { ...state, message: action.payload };
+      case GET_SUCCESS:
+        return { message: "", smurfs: action.payload };
+      case GET_FAIL:
+        return { ...state, message: action.payload };
+      case ADD_SMURF:
+        return { ...state, message: action.payload };
+      case ADD_SUCCESS:
+        return { message: "Smurf added", smurfs: action.payload };
+      case ADD_FAIL:
+        return { ...state, message: action.payload };
+      default:
+        return state;
     }
   };
